91e01430c9ed7b9298bb7ead8e18d54f4faeb820
mustafadag525 <55752233+mustafadag525@users.noreply.github.com>
2020-08-05 01:37:16 +0300

    Add files via upload

--- rastgele sayı üretme.c
+++ rastgele sayı üretme.c
@@ -1,19 +1,25 @@
-include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-
-void zar_at()
+void sayisal_loto()
 {
-
     srand(time(NULL));
-    int deger=1+rand()%10;
-    printf("%d",deger);
+    for(int i=1;i<=6;i++)
+    {
+        printf("%d. kolon: ",i);
+        for(int j=1;j<=6;j++)
+        {
+            printf("%d\t",1+rand()%45);
+        }
+        printf("\n");
+    }
 }
-
 
 int main()
 {
-zar_at();
 
+sayisal_loto();
+
+    return 0;
 }

8b9780fb1351778e59b939b6193789f0bb1b0c7b
mustafadag525 <55752233+mustafadag525@users.noreply.github.com>
2020-11-09 12:45:15 +0300

    Add files via upload

---
+++ yıldız karakteriyle M harfi.c
@@ -0,0 +1,62 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+int main()
+{
+
+
+
+    printf("-----------------------\n\n\n");
+    printf("AD:\n\n\n");
+
+
+    int i,j,k,a,l,b;
+
+    for(i=0;i<=11;i++)  //M HARFI;
+    {
+        for(j=0;j<=2;j++)
+        {
+            printf("*");
+        }
+
+    for(k=1;k<=i;k++)
+    {
+        printf(" ");
+    }
+
+    for(j=0;j<=2;j++)
+    {
+        printf("*");
+    }
+
+    for(a=10;a>=i;a--)
+    {
+        printf("  ");
+    }
+
+    for(j=0;j<=2;j++)
+    {
+        printf("*");
+    }
+
+
+    for(k=1;k<=i;k++)
+    {
+        printf(" ");
+    }
+
+    for(j=0;j<=2;j++)
+    {
+        printf("*");
+    }
+    printf("\n");
+
+
+    }
+printf("\n\n\n");
+
